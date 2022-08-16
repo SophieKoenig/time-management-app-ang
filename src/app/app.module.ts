683e6d519ddf4c1f24df010923242847fd062c3e
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-08-16 10:41:00 +0200

    created to-do list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './timer/timer.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimerComponent
+    TimerComponent,
+    ToDoListComponent
   ],
   imports: [
     BrowserModule,
